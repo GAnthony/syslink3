714481f2a673f72a5b730d22f54582f87407673d
Mark Grosen <mgrosen@ti.com>
2012-07-11 17:45:53 -0700

    update MessageQBench to specify number of loops in initial message

--- src/tests/MessageQBench.c
+++ src/tests/MessageQBench.c
@@ -37,18 +37,24 @@
  *  ============================================================================
  */
 
-/* Standard headers */
+/*
+ * opkg --tmp-dir ~/tmp install util-linux-ng
+ * chrt -f ./MessageQBench
+ */
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <sys/param.h>
 
-/* SysLink/IPC Headers: */
 #include <Std.h>
 #include <SysLink.h>
 #include <ti/ipc/MessageQ.h>
 
+#define MessageQ_payload(m) ((void *)((char *)(m) + sizeof(MessageQ_MsgHeader)))
+#define MINPAYLOADSIZE      (2 * sizeof(UInt32))
+
 /* App defines: Must match on remote proc side: */
 #define NUM_LOOPS           1000  /* Number of transfers to be tested. */
-#define MSGSIZE             64u
 #define HEAPID              0u
 #define CORE0_MESSAGEQNAME  "SLAVE"
 #define MPU_MESSAGEQNAME    "HOST"
@@ -56,37 +62,36 @@
 
 long diff(struct timespec start, struct timespec end)
 {
-    long    usecs;
-
     struct timespec temp;
-    if ((end.tv_nsec-start.tv_nsec)<0) {
-        temp.tv_sec = end.tv_sec-start.tv_sec-1;
-        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+
+    if ((end.tv_nsec - start.tv_nsec) < 0) {
+        temp.tv_sec = end.tv_sec - start.tv_sec-1;
+        temp.tv_nsec = 1000000000UL + end.tv_nsec - start.tv_nsec;
     } else {
-        temp.tv_sec = end.tv_sec-start.tv_sec;
-        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+        temp.tv_sec = end.tv_sec - start.tv_sec;
+        temp.tv_nsec = end.tv_nsec - start.tv_nsec;
     }
-    usecs = temp.tv_sec * 1000000 + temp.tv_nsec / 1000;
-    return usecs;
+
+    return (temp.tv_sec * 1000000UL + temp.tv_nsec / 1000);
 }
 
-Int
-MessageQApp_execute ()
+Int MessageQApp_execute(UInt32 numLoops, UInt32 payloadSize)
 {
     Int32                    status     = 0;
     MessageQ_Msg             msg        = NULL;
     MessageQ_Params          msgParams;
-    int                      i;
+    UInt16                   i;
     MessageQ_QueueId         queueId = MessageQ_INVALIDMESSAGEQ;
     MessageQ_Handle          msgqHandle;
-    struct timespec          start,end;
-    long                     elapsed=0;
+    struct timespec          start, end;
+    long                     elapsed;
+    UInt32 *params;
 
     printf ("Entered MessageQApp_execute\n");
 
     /* Create the local Message Queue for receiving. */
-    MessageQ_Params_init (&msgParams);
-    msgqHandle = MessageQ_create (MPU_MESSAGEQNAME, &msgParams);
+    MessageQ_Params_init(&msgParams);
+    msgqHandle = MessageQ_create(MPU_MESSAGEQNAME, &msgParams);
     if (msgqHandle == NULL) {
         printf ("Error in MessageQ_create\n");
         goto exit;
@@ -96,52 +101,58 @@ MessageQApp_execute ()
             MessageQ_getQueueId(msgqHandle));
     }
 
-    /* Poll until remote side has it's messageQ created before we send: */
+    /* Poll until remote side has its messageQ created before we send: */
     do {
-        status = MessageQ_open (CORE0_MESSAGEQNAME, &queueId);
+        status = MessageQ_open(CORE0_MESSAGEQNAME, &queueId);
         sleep (1);
     } while (status == MessageQ_E_NOTFOUND);
+
     if (status < 0) {
-        printf ("Error in MessageQ_open [0x%x]\n", status);
+        printf("Error in MessageQ_open [0x%x]\n", status);
         goto cleanup;
     }
-    else {
-        printf ("Remote queueId  [0x%x]\n", queueId);
-    }
 
-    /* Allocate message. */
-    msg = MessageQ_alloc (HEAPID, MSGSIZE);
+    printf("Remote queueId  [0x%x]\n", queueId);
+
+    msg = MessageQ_alloc(HEAPID, sizeof(MessageQ_MsgHeader) + payloadSize);
     if (msg == NULL) {
-        printf ("Error in MessageQ_alloc\n");
-        MessageQ_close (&queueId);
+        printf("Error in MessageQ_alloc\n");
+        MessageQ_close(&queueId);
         goto cleanup;
     }
-    printf ("\nExchanging %d messages with remote processor...\n", NUM_LOOPS);
 
-    for (i = 0 ; i < NUM_LOOPS ; i++) {
-        if (1 == i) {
-            /* Start after first sleep() call in BIOS side messageq sample: */
-            clock_gettime(CLOCK_REALTIME, &start);
-        }
-        MessageQ_setMsgId (msg, i);
+    /* handshake with remote to set the number of loops */
+    MessageQ_setReplyQueue(msgqHandle, msg);
+    params = MessageQ_payload(msg);
+    params[0] = numLoops;
+    params[1] = FALSE;
+    MessageQ_put(queueId, msg);
+    MessageQ_get(msgqHandle, &msg, MessageQ_FOREVER);
+
+    printf("Exchanging %d messages with remote processor...\n", numLoops);
+
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    for (i = 0; i < numLoops; i++) {
+        MessageQ_setMsgId(msg, i);
 
         /* Have the remote proc reply to this message queue */
-        MessageQ_setReplyQueue (msgqHandle, msg);
+        MessageQ_setReplyQueue(msgqHandle, msg);
 
-        status = MessageQ_put (queueId, msg);
+        status = MessageQ_put(queueId, msg);
         if (status < 0) {
-            printf ("Error in MessageQ_put [0x%x]\n", status);
+            printf("Error in MessageQ_put [0x%x]\n", status);
             break;
         }
 
         status = MessageQ_get(msgqHandle, &msg, MessageQ_FOREVER);
         if (status < 0) {
-            printf ("Error in MessageQ_get [0x%x]\n", status);
+            printf("Error in MessageQ_get [0x%x]\n", status);
             break;
         }
         else {
             /* Validate the returned message. */
-            if ((msg != NULL) && (MessageQ_getMsgId (msg) != i)) {
+            if ((msg != NULL) && (MessageQ_getMsgId(msg) != i)) {
                 printf ("Data integrity failure!\n"
                         "    Expected %d\n"
                         "    Received %d\n",
@@ -150,48 +161,52 @@ MessageQApp_execute ()
             }
         }
     }
+
     clock_gettime(CLOCK_REALTIME, &end);
-    elapsed = diff(start,end);
+    elapsed = diff(start, end);
 
-    status = MessageQ_free (msg);
-    if (status < 0) {
-            printf ("Error in MessageQ_get [0x%x]\n", status);
+    if (numLoops > 0) {
+        printf("Avg round trip time: %ld usecs\n", elapsed / numLoops);
     }
 
-    printf ("Sample application successfully completed!\n");
-
-    MessageQ_close (&queueId);
-    if (i > 1) {
-        printf("Avg round trip time: %ld usecs\n", elapsed/(i-1));
-    }
+    MessageQ_free(msg);
+    MessageQ_close(&queueId);
 
 cleanup:
-    /* Clean-up */
-    status = MessageQ_delete (&msgqHandle);
+    status = MessageQ_delete(&msgqHandle);
     if (status < 0) {
         printf ("Error in MessageQ_delete [0x%x]\n", status);
     }
 
 exit:
-    printf ("Leaving MessageQApp_execute\n\n");
+    printf("Leaving MessageQApp_execute\n\n");
 
     return (status);
 }
 
-int
-main (int argc, char ** argv)
+int main (int argc, char * argv[])
 {
-    Int32   status = 0;
+    Int32 status = 0;
+    UInt32 numLoops = NUM_LOOPS;
+    UInt32 payloadSize = MINPAYLOADSIZE;
+
+    if (argc > 1) {
+        numLoops = strtoul(argv[1], NULL, 0);
+    }
+
+    if (argc > 2) {
+        payloadSize = MAX(strtoul(argv[2], NULL, 0), MINPAYLOADSIZE);
+    }
 
     status = SysLink_setup();
 
     if (status >= 0) {
-        MessageQApp_execute();
+        MessageQApp_execute(numLoops, payloadSize);
         SysLink_destroy();
     }
     else {
-        printf ("SysLink_setup failed: status = 0x%x\n", status);
+        fprintf(stderr, "SysLink_setup failed: status = 0x%x\n", status);
     }
 
-    return(0);
+    return (status);
 }
